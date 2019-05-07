503034b460e5e2b5a3cdeb73ec1e7c02b055cbc9
Quentin Strinati <qstrinati@gmail.com>
2019-05-07 09:54:03 +0200

    Commit Propre

---
+++ Assets/Scripts/PortalController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour
+{
+
+    int count = 0;
+    public GameObject portalIn;
+    public GameObject portalOut;
+    //public GameObject cube;
+
+    void teleport(GameObject portalIn, GameObject portalOut, GameObject cube)
+    {
+        GameObject newCube = Instantiate(cube, new Vector3(portalOut.transform.localPosition.x, portalOut.transform.localPosition.y - .2f, portalOut.transform.localPosition.z), Quaternion.identity);
+        newCube.GetComponent<Rigidbody>().AddForce(cube.GetComponent<Rigidbody>().velocity);
+        Debug.Log("cube" + count.ToString() + cube.GetComponent<Rigidbody>().velocity + "\n");
+        Debug.Log("newCube" + count.ToString() + newCube.GetComponent<Rigidbody>().velocity + "\n");
+        count++;
+        Destroy(cube);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("other.gameObject.GetComponent<Rigidbody>().velocity = " + other.gameObject.GetComponent<Rigidbody>().velocity);
+        
+        teleport(portalIn, portalOut, other.gameObject);
+    }
+}
---
+++ Assets/Scripts/WinController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WinController : MonoBehaviour
+{
+
+    public Text winText;
+    void Awake()
+    {
+        winText.enabled = false;
+    }
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        winText.enabled = true;
+    }
+}
